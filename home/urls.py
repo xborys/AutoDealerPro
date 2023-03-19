481529634a1189a3630ef498c09a05b9b7c2b7ae
xBorys <borysek321@gmail.com>
2023-03-19 17:49:49 +0100

    naprawa FAQ

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
-]
-
-urlpatterns = [
-    path('', views.home, name='FAQ')
+    path('', views.home, name='home'),
+    path('FAQ', views.FAQ, name='FAQ'),
 ]
