d20da8db7faaa801e53d739b2cd0a1afb7abac15
xBorys <borysek321@gmail.com>
2023-03-26 15:55:47 +0200

    head, footer html

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('FAQ', views.FAQ, name='FAQ'),
     path('index', views.index, name='index'),
     
