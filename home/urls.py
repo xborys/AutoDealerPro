006562d896fbe3de6c8632acef0b4eb47ec85268
PatrykDreja <patryk.piotr.dreja@gmail.com>
2023-03-29 22:06:29 +0200

    ChangeToHeadIndexCarlist

--- home/urls.py
+++ home/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('FAQ', views.FAQ, name='FAQ'),
     path('index', views.index, name='index'),
-<<<<<<< HEAD
-    path('CarList', views.index, name='CarList'),
-    path('Partners', views.Partners, name='Partners')
-=======
+    path('CarList', views.CarList, name='CarList'),
     path('LP',views.LP,name='LP'),
     
->>>>>>> a669718d512b5e55b0d1e04e25997ed51698a297
 ]
--- home/views.py
+++ home/views.py
@@ -9,13 +9,9 @@ def FAQ(request):
 def index(request):
     return render(request, 'index.html', {})
 
-<<<<<<< HEAD
 def CarList(request):
     return render(request, 'CarList.html', {})
 
-def Partners(request):
-    return render(request, 'Partners.html', {})
-=======
 def LP(request):
     return render(request, 'partnerzy.html', {})
->>>>>>> a669718d512b5e55b0d1e04e25997ed51698a297
+
