9b0fd8583d5342742ddbe3b8b6d8b8050308fb07
xBorys <borysek321@gmail.com>
2023-04-04 21:10:13 +0200

    add login system

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
